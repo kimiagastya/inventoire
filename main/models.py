ea34aeacb947567e9f5590290e792d3373cf26eb
kimiagastya <kimiagastya@gmail.com>
2023-10-03 22:51:43 +0700

    Update Tugas 5

--- main/models.py
+++ main/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    # nama_identitas = models.TextField()
-    # kelas_identitas = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
