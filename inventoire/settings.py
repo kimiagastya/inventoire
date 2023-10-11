0d1e6f3070b6cb39995cc4b2d86190976c7b44d1
kimiagastya <kimiagastya@gmail.com>
2023-10-11 21:56:28 +0700

    Change STATIC settings.py

--- inventoire/settings.py
+++ inventoire/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join("inventoire", 'static')
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main/static'),
 ]
